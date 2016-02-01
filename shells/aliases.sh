3ff8b9a1a07cdfe583cd379c10a8a09c62e2132e
Milan Arandjelovic <milanarandjelovic90@gmail.com>
2016-02-01 13:59:35 +0100

    Edit aliases

--- shells/aliases.sh
+++ shells/aliases.sh
@@ -1,13 +1,18 @@
 # Aliases.
+#==============================
 alias l='ls -alF'
 
 # Tmux
+#==============================
 alias tm='tmux -2'
 
 # git
-alias gs='git status -s'
-alias gc='git commit'
-alias gp='git push origin'
+#==============================
+alias gst='git status -s'
+alias gc='git commit -m'
+alias ga='git add'
+alias gaa='git add -A'
+alias gp='git push -u origin master'
 alias gpr='git pull --rebase'
 alias gpu='git pull'
 
@@ -17,6 +22,7 @@ alias ag='/usr/bin/ag'
 alias t='phpunit'
 
 # conditional alias
+#==============================
 which vim.gnome > /dev/null 2>&1 && alias vim=vim.gnome
 which mvim > /dev/null 2>&1 && alias vim='mvim -v'
 which mvim > /dev/null 2>&1 && alias gvim='mvim -v'
