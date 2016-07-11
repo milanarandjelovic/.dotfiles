421d09df06de33ad3842fdaf0d302be8d7c975df
Milan Arandjelovic <milanarandjelovic@users.noreply.github.com>
2016-07-11 23:26:04 +0200

    Update include_path.sh

--- shells/include_path.sh
+++ shells/include_path.sh
@@ -2,6 +2,6 @@ PATH=$HOME/.rvm/bin:$PATH # Add RVM to PATH for scripting
 PATH=$HOME/.dotfiles/bin:$PATH
 PATH=./vendor/bin:$PATH # for php composer binaries
 PATH=$HOME/node_modules/.bin:$PATH
-PATH=$HOME/.composer/vendor/bin:$PATH
+export PATH="$HOME/.config/composer/vendor/bin:$PATH" # for Laravel and Lumen
 PATH=$HOME/.bin:$PATH
 # PATH=$PATH:$HOME/.rubinius/bin
